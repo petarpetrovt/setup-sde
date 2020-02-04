9cbb9cabb5abf648444855dc0c7437420d01fd75
Petar Trifonov Petrov <petarpetrovt@gmail.com>
2020-02-04 21:50:47 +0200

    Asd

--- .github/actions/setup-sde/build.js
+++ .github/actions/setup-sde/build.js
@@ -1,22 +1,4 @@
-const core = require("@actions/core");
-const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
 
-try {
-    exec('npm install', (error, stdout, stderr) => {
-        if (error) {
-            console.log(stdout);
-            console.log(stderr);
-            throw new Error(`NPM INSTALL ERROR: ${error}`);
-        }
-
-        exec('node index.js', (error1, stdout1, stderr1) => {
-            if (error1) {
-                console.log(stdout1);
-                console.log(stderr1);
-                throw new Error(`NODE INDEX.JS ERROR: ${error1}`);
-            }
-        });
-    });
-} catch (error) {
-    core.setFailed(error.message);
-}
+execSync('npm install');
+execSync('node index.js');
