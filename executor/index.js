1c8fe4994bcf3ef3a367ecbe551665147214eaa0
Petar Petrov <ppetrov@david.bg>
2020-02-05 15:20:09 +0200

    Fix paths

--- executor/index.js
+++ executor/index.js
@@ -4,7 +4,9 @@ const exec = require('child_process').exec;
 
 function npmInstall(packagePath) {
     return new Promise((resolve, reject) => {
-        exec(`npm install ${packagePath}`, (error, stdout, stderr) => {
+        exec(`npm install`, {
+            cwd: packagePath
+        }, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
                 return;
@@ -15,9 +17,11 @@ function npmInstall(packagePath) {
     });
 }
 
-function getSDEPath(packageIndexPath) {
+function getSDEPath(packagePath, packageIndexFileName) {
     return new Promise((resolve, reject) => {
-        exec(`node ${packageIndexPath}`, (error, stdout, stderr) => {
+        exec(`node ${packageIndexFileName}`, {
+            cwd: packagePath
+        }, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
                 return;
@@ -40,13 +44,13 @@ async function run() {
         }
 
         // TODO: pass directory
-        const directory = path.join(__dirname, `../../installer`);
-        const packageIndexPath = path.join(directory, `index.js`);
+        const packagePath = path.join(__dirname, `../../installer`);
+        const packageIndexFileName = `index.js`;
 
         // TODO: improve info logging
-        await npmInstall(directory);
+        await npmInstall(packagePath);
 
-        const sdePath = await getSDEPath(packageIndexPath);
+        const sdePath = await getSDEPath(packagePath, packageIndexFileName);
 
         core.exportVariable(environmentVariableName, sdePath);
     }
