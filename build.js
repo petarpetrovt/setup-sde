6d5388a8bc86ca8f95c4e76659038ffbf0ed40cf
Petar Petrov <ppetrov@david.bg>
2020-02-05 09:28:52 +0200

    asd

--- build.js
+++ build.js
@@ -1,4 +1,13 @@
 const execSync = require('child_process').execSync;
 
-execSync('npm install');
-execSync('node index.js');
+console.log(`Starting NPM install...`);
+execSync('npm install', {
+    stdio: 'inherit'
+});
+console.log(`Done.`);
+
+console.log(`Starting NODE index.js...`);
+execSync('node index.js', {
+    stdio: 'inherit'
+});
+console.log(`Done.`);
