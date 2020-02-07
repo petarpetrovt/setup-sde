6b23fa29c3ba8622886685cbd5184c51b036a5bf
Petar Trifonov Petrov <petarpetrovt@gmail.com>
2020-02-07 20:18:08 +0200

    Fix node.js SDE path variable exporting

--- executor/CommandExecutor.ts
+++ executor/CommandExecutor.ts
@@ -1,28 +1,19 @@
-import * as core from '@actions/core';
 import { exec } from '@actions/exec';
 import { ExecOptions } from '@actions/exec/lib/interfaces';
 import path from 'path';
 
 export class CommandExecutor {
     private readonly _directory: string;
-    private readonly _sdePathPrefix: string;
-    public sdePath: string | null;
 
-    private constructor(directory: string, sdePathPrefix: string) {
+    private constructor(directory: string) {
         if (typeof directory !== "string" || directory.length <= 0) {
             throw new Error(`Invalid command executor directory.`);
         }
 
-        if (typeof sdePathPrefix !== "string" || sdePathPrefix.length <= 0) {
-            throw new Error(`Invalid SDE path prefix.`);
-        }
-
         this._directory = directory;
-        this._sdePathPrefix = sdePathPrefix;
-        this.sdePath = null;
     }
 
-    public async execute(value: string, args: string[] | undefined, processOutput: boolean = false, requiresXvfb: boolean = false): Promise<void> {
+    public async execute(value: string, args: string[] | undefined, requiresXvfb: boolean = false): Promise<void> {
         if (typeof value !== "string" || value.length <= 0) {
             throw new Error(`Can't execute empty command.`);
         }
@@ -31,13 +22,6 @@ export class CommandExecutor {
 
         if (!requiresXvfb) {
             options.cwd = this._directory;
-        }
-
-        if (processOutput) {
-            options.listeners = {
-                stdout: (data: any) => this.processOutput(data),
-                stderr: (data: any) => this.processError(data)
-            };
         }
 
         const isLinux: boolean = process.platform === "linux";
@@ -76,30 +60,8 @@ export class CommandExecutor {
         }
     }
 
-    private processOutput(value: any): void {
-        if (value) {
-            core.warning(`OUTPUT: ${value.toString()}`);
-
-            const valueString: string = value.toString();
-            if (valueString.startsWith(this._sdePathPrefix)) {
-                this.sdePath = valueString
-                    .substring(this._sdePathPrefix.length)
-                    .trim();
-            }
-            else {
-                core.debug(valueString);
-            }
-        }
-    }
-
-    private processError(value: any): void {
-        if (value) {
-            core.error(value.toString());
-        }
-    }
-
-    public static async create(directory: string, sdePathPrefix: string): Promise<CommandExecutor> {
-        const result = new CommandExecutor(directory, sdePathPrefix);
+    public static async create(directory: string): Promise<CommandExecutor> {
+        const result = new CommandExecutor(directory);
 
         await result.prepare();
 
--- executor/index.ts
+++ executor/index.ts
@@ -2,34 +2,20 @@ import * as core from '@actions/core';
 import * as path from 'path';
 import { CommandExecutor } from './CommandExecutor';
 
-const installerDirectoryPath = path.join(__dirname, "../../installer");
-const installerFileName = "index.js";
-const installerSdePathPrefix = `SDE_PATH:`;
+const installerDirectoryPath = path.join(__dirname, '../../installer');
+const installerFileName = 'index.js';
 
 async function run(): Promise<void> {
     try {
-        const environmentVariableName = core.getInput("environmentVariableName") || "SDE_PATH";
-        core.debug(`environmentVariableName: ${environmentVariableName}`);
+        core.debug(`Installer path: ${installerDirectoryPath}`);
 
-        if (!environmentVariableName || environmentVariableName.length <= 0) {
-            core.setFailed("Missing enviroment variable name.");
-            return;
-        }
+        const executor: CommandExecutor = await CommandExecutor.create(installerDirectoryPath);
 
-        core.warning(`Installer path: ${installerDirectoryPath}`);
-
-        const executor: CommandExecutor = await CommandExecutor.create(installerDirectoryPath, installerSdePathPrefix);
-
-        await executor.execute("npm", ["install"]);
+        await executor.execute('npm', ['install']);
 
         const absolutePath: string = path.join(installerDirectoryPath, installerFileName);
-        await executor.execute("node", [absolutePath], true, true);
 
-        if (!executor.sdePath || executor.sdePath.length <= 0) {
-            throw new Error("Could not provide SDE binaries path.");
-        }
-
-        core.exportVariable(environmentVariableName, executor.sdePath);
+        await executor.execute('node', [absolutePath], true);
     }
     catch (e) {
         core.setFailed(`An error has occured while setuping SDE binaries: ${e.message}`);
