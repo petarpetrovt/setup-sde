1f018415ba970383e6a7352c026591742cc6b7d4
Petar Petrov <ppetrov@david.bg>
2020-12-15 10:18:06 +0200

    Improve CI

--- executor/tester.js
+++ executor/tester.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const core = require('@actions/core');
+const path = require("path");
 
 try {
     const environmentVariableName = process.argv[2];
@@ -14,6 +16,26 @@ try {
     } else {
         core.info(`Succesfly asserted environment variable with name '${environmentVariableName}' and value '${environmentVariableValue}'.`);
     }
+
+    if (!fs.existsSync(environmentVariableValue)) {
+        core.setFailed(`Path '${environmentVariableValue}' doesn't exist.`);
+    }
+
+    try {
+        const folderPath = path.join(process.cwd(), ...environmentVariableValue.split("/"));
+        const filePaths = fs
+            .readdirSync(folderPath, { withFileTypes: true })
+            .filter((dirent) => dirent.isFile())
+            .map((dirent) => dirent.name.split(".")[0]);
+
+        core.info(`Directory files:`);
+        filePaths.forEach(filePath => {
+            core.info(filePath);
+        });
+    } catch (err) {
+        core.setFailed(err);
+    }
+
 }
 catch (e) {
     core.warning(e.message);
