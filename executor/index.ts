3f563d89d3f3da29f303c8b876089d1708ea0dc0
Petar Petrov <ppetrov@david.bg>
2021-03-03 11:25:24 +0200

    Minor updates

--- executor/index.ts
+++ executor/index.ts
@@ -2,20 +2,23 @@ import * as core from '@actions/core';
 import * as path from 'path';
 import { CommandExecutor } from './CommandExecutor';
 
-const installerDirectoryPath = path.join(__dirname, '../../installer');
 const installerFileName = 'index.js';
+const installerDirectoryPath = path.join(__dirname, '../../installer');
+const installerPath = path.join(installerDirectoryPath, installerFileName);
 
 async function run(): Promise<void> {
     try {
         core.debug(`Installer path: ${installerDirectoryPath}`);
 
         const executor: CommandExecutor = await CommandExecutor.create(installerDirectoryPath);
+        await executor.execute('npm', ['install'], false);
+        await executor.execute('node', [installerPath], true);
 
-        await executor.execute('npm', ['install']);
-
-        const absolutePath: string = path.join(installerDirectoryPath, installerFileName);
-
-        await executor.execute('node', [absolutePath], true);
+        if (process.platform != "win32") {
+            // chmod -R +x
+            const executor2: CommandExecutor = await CommandExecutor.create(installerDirectoryPath);
+            executor2.execute(`chmod`, ['-R', '+x'], false);
+        }
     }
     catch (e) {
         core.setFailed(`An error has occured while setuping SDE binaries: ${e.message}`);
