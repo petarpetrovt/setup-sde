c9e81cca79bb243572672e3650d40ce9eabfaf3a
Petar Petrov <ppetrov@david.bg>
2021-09-07 23:17:57 +0300

    Refactor action to use actions/tool-cache package

--- index.ts
+++ index.ts
@@ -1,34 +1,11 @@
 import * as exec from '@actions/exec';
 import * as core from '@actions/core';
+import * as tool from '@actions/tool-cache';
 import * as path from 'path';
-import _7z from '7zip-min';
 import fs from 'fs';
-import fetch from 'node-fetch';
 
 const defaultEnvironmentVariableName: string = "SDE_PATH";
 const defaultSdeVersion: string = "8.69.1-2021-07-18";
-
-function unzip(tarBzPath: string, tarPath: string, outputDir: string): Promise<string> {
-    const filesPath: string = path.join(outputDir, `sde-temp-files`);
-
-    return new Promise<string>((resolve, reject) => {
-        _7z.unpack(tarBzPath, outputDir, (err: any) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-
-            _7z.unpack(tarPath, filesPath, (err: any) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                resolve(filesPath);
-            });
-        });
-    });
-}
 
 function getPlatformIdentifier(): string {
     switch (process.platform) {
@@ -62,76 +39,38 @@ async function run(): Promise<void> {
 
         const platform: string = getPlatformIdentifier();
         const url: string = `https://software.intel.com/content/dam/develop/external/us/en/documents/downloads/sde-external-${sdeVersion}-${platform}.tar.bz2`;
-        const outputDir = path.resolve(`.output`);
-        const tarBzPath = path.join(outputDir, `sde-temp-file.tar.bz2`);
-        const tarPath = path.join(outputDir, `sde-temp-file.tar`);
-        const filesPath = path.join(outputDir, `sde-temp-files`);
+        const outputDirectory: string = path.resolve(`.output`);
+        const tarFilePath: string = path.join(outputDirectory, `sde-temp-file.tar.bz2`);
+        const extractedFilesPath: string = path.join(outputDirectory, `sde-temp-files`);
 
-        core.info("Ensuring output directory");
-        // Ensure output directory
-        await fs.promises.mkdir(outputDir, {
-            recursive: true
-        });
+        // Download tool
+        await tool.downloadTool(url, tarFilePath);
 
-        // Download archive
-        core.info("Fetching url");
-        const response = await fetch(url);
-        if (!response.ok || !response.body) {
-            core.setFailed(`Unexpected response: ${response.statusText}`);
-            return;
-        }
-
-        core.info("Parsing response");
-        const buffer: ArrayBuffer = await response.arrayBuffer();
-
-        core.info("Saving response");
-        await fs.promises.writeFile(tarBzPath, Buffer.from(buffer));
-
-        core.info("Ensuring file permissions");
         // Ensure file permissions
         if (process.platform != "win32") {
             await exec.exec(`sudo chmod`, ['-R', '777', __dirname]);
-            await exec.exec(`sudo chmod`, ['-R', '777', outputDir]);
+            await exec.exec(`sudo chmod`, ['-R', '777', outputDirectory]);
         }
 
-        // Unzip archive
-        core.info("Unzipping");
-        let unzipedDirectory: string;
-        if (process.platform != "win32") {
-            unzipedDirectory = path.join(outputDir, `sde-temp-files`);
+        // Extract tool
+        await tool.extractTar(tarFilePath, extractedFilesPath);
 
-            // Ensure output directory
-            await fs.promises.mkdir(unzipedDirectory, {
-                recursive: true
-            });
-
-            // unzip via tar command
-            await exec.exec(`tar`, ['xvf', tarBzPath, '-C', unzipedDirectory]);
-        } else {
-            // unzip via 7zip command
-            unzipedDirectory = await unzip(tarBzPath, tarPath, outputDir);
-        }
-
-        core.info("Done1");
-        const filesPaths: string[] = await fs.promises.readdir(unzipedDirectory);
-
-        core.info("Done2");
+        // Ensure export path
+        const filesPaths: string[] = await fs.promises.readdir(extractedFilesPath);
         if (filesPaths && filesPaths.length === 1) {
             // Ensure unzip directory permissions
             if (process.platform != "win32") {
-                await exec.exec(`sudo chmod`, ['-R', '777', outputDir]);
+                await exec.exec(`sudo chmod`, ['-R', '777', outputDirectory]);
             }
 
             // Export SDE path
-            const sdePath = path.join(filesPath, filesPaths[0]);
+            const sdePath = path.join(extractedFilesPath, filesPaths[0]);
             core.exportVariable(environmentVariableName, sdePath);
         } else {
             core.setFailed(`Failed to get SDE path.`);
         }
     }
     catch (e: any) {
-        console.log(e);
-
         core.setFailed(`An error has occurred while setuping SDE binaries.`);
         core.error(e);
     }
